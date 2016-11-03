547bdf835f79cdc8ecef09ae4454e1d63814b2fc
Stefan Kuburovic <stefan.kuburovic@sentinelactive.com>
2016-11-03 17:26:55 +0100

    Second commit - connecting API

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-var basic_choropleth = new Datamap({
+
+var githubMap = new Datamap({
   element: document.getElementById("worldMap"),
   projection: 'mercator',
   fills: {
@@ -8,13 +9,47 @@ var basic_choropleth = new Datamap({
 
 var colors = d3.scale.category10();
 var countries = Datamap.prototype.worldTopo.objects.world.geometries;
+var countriesRepacked = [];
+var stashResult = {};
+$.each(countries, function(index, value){
+	countriesRepacked.push({id: value.id, name: value.properties.name});
+});
 
-function randomCountry() {
-	return countries[Math.floor(Math.random()*countries.length)].properties.iso;
-}
+githubCountries();
+setInterval(function(){
+	$('.avatars').html('');
+	githubCountries();
+}, 60000);
 
-setInterval(function() {
-  basic_choropleth.updateChoropleth({
-	  [randomCountry()] : colors(Math.random() * 100)
-	});
-}, 2000);
+	function githubCountries(){
+		 $.ajax({
+		 	url: "https://api.github.com/events",
+		 	success: function(result){
+		 		if(result != stashResult) {
+			 		$.each(result, function(index, value){
+			 			$('.avatars').css('display', 'none');
+						$('.avatars').append('<a href="' + value.actor.url.replace('api.', '').replace('/users', '') + '"><img src="' + value.actor.avatar_url + '" title="' + value.actor.url.replace('api.', '').replace('/users', '') + '"/></a>');
+						setTimeout(function(){
+			 				$('.avatars').fadeIn(1000).css('display', 'block').fadeIn(1000);
+						}, 2000);
+						$.ajax({
+							url: value.actor.url + "?client_id=90f2bc697417b6c1d3b9&client_secret=59bcaaa0e61b1c1943e02d9f8c41313a5edcca8f",
+							success: function(result) {
+								console.log(result);
+								if(result.location != null) {
+									$.each(countriesRepacked, function(index, value){
+										if(result.location.includes(value.name)) {
+											var iso = value.id;
+											 githubMap.updateChoropleth({
+											  [iso]: '#17becf'
+											});
+										}
+									});
+								}
+							}
+						});
+			 		});
+		 		}
+			}
+		});
+	}
